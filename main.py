9c3723471ec8fa893065cf52a9213d0b42bfc991
akli hiba <ih_akli@esi.dz>
2022-01-08 23:31:40 +0100

    TP1 TPGO

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 
 def knapSack(W, wt, val):
     n = len(val)
@@ -12,6 +11,7 @@ def knapSack(W, wt, val):
                 table[i][j] = max(val[i - 1] + table[i - 1][j - wt[i - 1]], table[i - 1][j])
             else:
                 table[i][j] = table[i - 1][j]
+    print(table)
     res = table[n][W]
     w = W
     for i in range(n, 0, -1):
@@ -27,8 +27,4 @@ def knapSack(W, wt, val):
     return table[n][W],optimal_items
 
 
-val = [50, 100, 150, 200]
-wt = [8, 16, 32, 40]
-W = 64
 
-#print(knapSack(W, wt, val))
